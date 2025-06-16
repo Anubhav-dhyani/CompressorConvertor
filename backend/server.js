fe20c8bf2761c706962130fe75624c9763333822
Anubhav-dhyani <adhyani1105@gmail.com>
2025-06-16 22:57:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,12 @@ app.use(express.json());
 app.use('/api/files', fileRoutes);
 
 app.listen(5000, () => console.log('Server running on port 5000'));
+const path = require('path');
+
+// Serve static files from React
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Fallback to index.html for any unknown routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
